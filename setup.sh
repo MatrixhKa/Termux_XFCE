3485810bedab14aa7265074c597f9eb12fbbeacd
MatrixhKa <123204683+MatrixhKa@users.noreply.github.com>
2024-06-12 07:01:41 -0300

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -74,7 +74,7 @@ proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 cp /usr/share/zoneinf
 
 setup_xfce() {
 #Install xfce4 desktop and additional packages
-pkg install git neofetch virglrenderer-android papirus-icon-theme xfce4 xfce4-goodies eza pavucontrol-qt bat jq nala wmctrl chromium netcat-openbsd termux-x11-nightly eza -y
+pkg install git neofetch virglrenderer-android papirus-icon-theme xfce4 xfce4-goodies eza pavucontrol-qt bat jq nala wmctrl netcat-openbsd termux-x11-nightly eza -y
 
 #Create .bashrc
 cp $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/etc/skel/.bashrc $HOME/.bashrc
@@ -99,10 +99,6 @@ alias upgrade='nala upgrade'
 alias install='nala install $@'
 alias uninstall='nala remove $@ '
 " >> $HOME/.bashrc
-
-#Put chromium icon on Desktop
-cp $HOME/../usr/share/applications/chromium.desktop $HOME/Desktop 
-chmod +x $HOME/Desktop/chromium.desktop
 
 cat <<'EOF' > ../usr/bin/prun
 #!/bin/bash
